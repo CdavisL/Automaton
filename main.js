0eeff106609aa4bda57b0d32441519e81c50f0da
CdavisL <cheriseldavis@gmail.com>
2018-12-17 09:32:12 -0600

    Commit

---
+++ main.js
@@ -0,0 +1,176 @@
+//Go through the chapter line by line and understand what each code does.
+//Can it be improved?
+
+//This is an array that lists the locations the robot will go to deliver packages.
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+
+//This method breaks down the above array to create a "map" that tells the robot what building is on each road.
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+
+//This method condenses the village to it's minimal set of values. Then activates the robot to start it's operation. 
+const roadGraph = buildGraph(roads);
+
+  var VillageState = class VillageState {
+    constructor(place, parcels) {
+      this.place = place;
+      this.parcels = parcels;
+    }
+  
+    move(destination) {
+      if (!roadGraph[this.place].includes(destination)) {
+        return this;
+      } else {
+        let parcels = this.parcels.map(p => {
+          if (p.place != this.place) return p;
+          return {place: destination, address: p.address};
+        }).filter(p => p.place != p.address);
+        return new VillageState(destination, parcels);
+      }
+    }
+  }
+
+
+//These call the method above. This allows the robot to move, deliver and then move again. These have been dummied out since they are not necessary for operation.
+/* let first = new VillageState(
+  "Post Office",
+  [{place: "Post Office", address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+// → Alice's House
+console.log(next.parcels);
+// → []
+console.log(first.place);
+// → Post Office
+
+
+//This freezes the object. The best practice is let people know not to mess with the object though. 
+let object = Object.freeze({value: 5});
+object.value = 10;
+console.log(object.value); */
+// → 5
+
+
+//This method lets the robot run and keep a memory. This allows the robot return an object that contains the direction it wants to move in and a memory value that will be given back to it the next time it is called. 
+function runRobot(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+
+//This is the dumbest possible way to do this, but these function make the robot move and deliver at random. Obviously, this is inefficient, but will get the job done.
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return {direction: randomPick(roadGraph[state.place])};
+}
+//
+
+
+//This creates a new state with some parcels. This is a static method, made so by adding a property to the constructor.
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({place, address});
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+
+//This is a smarter route for the robot to take. The program works, but these next methods will make it efficient. 
+const mailRoute = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+];
+
+
+//This method allows the robot to use it's memory and follow the route.
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return {direction: memory[0], memory: memory.slice(1)};
+}
+
+
+//This method allows the robot to find the shortest route.
+function findRoute(graph, from, to) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+
+//This method allow the robot to navigate from any two points. Any location on the graph/map can be reached from any point on the same graph/map.
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
+
+
+//This executes the whole app. This is random.
+//runRobot(VillageState.random(), randomRobot);
+
+//This runs the route robot (the more efficient one).
+//runRobot(VillageState.random(), routeRobot, []);
+
+//This is the most optimal version so far.
+runRobot(VillageState.random(), goalOrientedRobot, []);
